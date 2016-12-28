aa5c6139950e7d97e3bbdf4494a4bdc046b17463
asalhani <asalhani@outlook.com>
2016-12-28 11:07:13 +0300

    controller, filter, sort, bind

--- getting_started_scott_allen/app/app.js
+++ getting_started_scott_allen/app/app.js
@@ -1,21 +1,45 @@
 var app = angular.module("app", []);
 
-app.controller('PersonController', ['$scope', '$http', function ($scope, $http) {
-    $scope.greeting = "hallow";
+app.controller('PersonController', ['$scope', '$http', function($scope, $http) {
+    $scope.greeting = "Github Viwer App!!";
     var person = {
         firstName: 'adib',
         lastName: 'salhani',
         imageSrc: ""
     }
-    $scope.person = person;
 
-    var onUserComplete = function (response) {
+    // PROPARTIES
+    $scope.person = person;
+    $scope.username = "angular";
+    $scope.repoSortOrder = '-stargazers_count';
+
+    // EVENT
+    var onUserComplete = function(response) {
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
     };
 
-    var onError = function(reason){
-        $scope.error = "error on calling HTTP !!!";
+    var onError = function(reason) {
+        $scope.error = "Could not fetch the data!!";
     }
-    $http.get('https://api.github.com/users/robconery')
-        .then(onUserComplete, onError);
+
+    // FUNCTIONS
+    $scope.search = function(username) {
+        $http.get('https://api.github.com/users/' + username)
+            .then(onUserComplete, onError);
+    }
+
+    var onRepos = function(response) {
+        $scope.repos = response.data;
+    }
+
+    // INIT
+    $scope.init = function() {
+        if ($scope.username.length > 0)
+            $scope.search($scope.username);
+    }
+
+    $scope.init();
+
 }]);
