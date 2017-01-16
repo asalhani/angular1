fa1caa16b010a9633be018027cc273eb07000735
asalhani <asalhani@outlook.com>
2017-01-16 11:43:10 +0300

    create GuessTheNumber app folder

---
+++ AngulaByExampleBook/GuessTheNumber/scripts/web-server.js
@@ -0,0 +1,22 @@
+/*
+ var express = require('express');
+ var path = require('path');
+ var app = express();
+ var rootPath = path.normalize(__dirname + '/../');
+ app.use(express.static(rootPath + '/app'));
+ app.listen(81);
+ console.log('Listening on port 8000...');
+ */
+
+var express = require('express');
+var app = express();
+var portNumber = 1025;
+
+app.use(express.static(__dirname + '/../'));
+
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+});
+
+app.listen(portNumber);
+console.log('Listening on port ' + portNumber + '...');
